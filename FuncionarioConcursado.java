5d2ac8fd5e351f810ccd93e5d4821be2e10ef375
jeffersoncasagrande <67493030+jeffersoncasagrande@users.noreply.github.com>
2020-07-27 23:04:21 -0300

    Primeiro Commit

---
+++ FuncionarioConcursado.java
@@ -0,0 +1,31 @@
+﻿
+public class FuncionarioConcursado {
+    
+    public int Codigo;
+    public double salarioBase;
+    public int anosContratacao;
+
+    public FuncionarioConcursado(int codigo, double salarioBase, int anosContratacao) {
+        this.codigo = codigo;
+        this.salarioBase = salarioBase;
+        this.anosContratacao = anosContratacao;
+    }
+    
+    public void imprimeDados(){
+        
+        System.out.println("===FUNCIONARIO CONCURSADO===");
+        System.out.println("CODIGO: " + this.codigo);
+        System.out.println("SALARIOBASE: " + this.salarioBase);
+        System.out.println("ANOSCONTRATACAO " + this.anos);
+        System.out.println("GRATIFICACAO: " + this.gratificacao());
+        
+    }
+    
+    public double gratificacao(){
+        return this.anos*200;
+    }
+    
+}
+
+
+
---
+++ FuncionarioTemporario.java
@@ -0,0 +1,30 @@
+﻿
+package exemplooo.etapa2;
+
+public class FuncionarioTemporario {
+    
+    public int Codigo;
+    public double salarioBase;
+    public int anosContratacao;
+
+    public FuncionarioConcursado(int codigo, double salarioBase, int anosContratacao) {
+        this.codigo = codigo;
+        this.salarioBase = salarioBase;
+        this.anosContratacao = anosContratacao;
+    }
+    
+    public void imprimeDados(){
+        
+        System.out.println("===FUNCIONARIO TEMPORARIO===");
+        System.out.println("CODIGO: " + this.codigo);
+        System.out.println("SALARIOBASE: " + this.salarioBase);
+        System.out.println("ANOSCONTRATACAO " + this.anos);
+        System.out.println("GRATIFICACAO: " + this.gratificacao());
+        
+    }
+    
+    public double gratificacao(){
+        return this.anos*15;
+    }
+    
+}
